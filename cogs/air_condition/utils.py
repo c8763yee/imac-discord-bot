8b3535695b6aae5340bd06cb19669b57d2284a72
c8763yee <rrrrr4413@gmail.com>
2024-04-30 12:28:00 +0800

    feat: add `stat` command for both `fan` and `Air_Conditioner`

--- cogs/air_condition/utils.py
+++ cogs/air_condition/utils.py
@@ -17,7 +17,7 @@ MQTT_PORT: int = int(os.environ.get("MQTT_PORT", 1883))
 logger = setup_package_logger(__name__)
 
 
-TURN_ON_AIRCONDITION_PAYLOAD = {"Status": "On", "Temp": "25"}
+TURN_ON_AIRCONDITION_PAYLOAD = {"Status": "On"}
 
 TURN_OFF_AIRCONDITION_PAYLOAD = {"Status": "Off"}
 
@@ -29,14 +29,20 @@ class AirConditionUtils(CogsExtension):
         async with Client(MQTT_BROKER, MQTT_PORT) as client:
             TURN_ON_AIRCONDITION_PAYLOAD["Temp"] = str(temperature)
             await client.publish(
-                f"{self.TOPIC}/switch", json.dumps(TURN_ON_AIRCONDITION_PAYLOAD)
+                f"{self.TOPIC}/control", json.dumps(TURN_ON_AIRCONDITION_PAYLOAD)
             )
         return f"Turned on aircondition with temperature {temperature}"
 
     async def turn_off(self) -> str:
         async with Client(MQTT_BROKER, MQTT_PORT) as client:
             await client.publish(
-                f"{self.TOPIC}/switch", json.dumps(TURN_OFF_AIRCONDITION_PAYLOAD)
+                f"{self.TOPIC}/control", json.dumps(TURN_OFF_AIRCONDITION_PAYLOAD)
             )
 
         return "Turned off aircondition"
+
+    async def get_stat(self) -> dict:
+        async with Client(MQTT_BROKER, MQTT_PORT) as client:
+            await client.subscribe(self.TOPIC)
+            async for message in client.messages:
+                return json.loads(message.payload)
